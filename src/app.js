68fd83d084d36a7c666a15ebe95b3526be3e3c4b
Sumit Raj <trapgamingpubg@gmail.com>
2024-01-23 14:48:00 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
-
+import cors from "cors";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello this is a response from server");
+// app.use(cors());
+
+app.get("/api/v1", (req, res) => {
+  res.send("this is home page");
+});
+
+app.get("/api/v1/name", (req, res) => {
+  res.send("hello my name is sumit");
 });
 
 export default app;
